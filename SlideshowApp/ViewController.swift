f92007ee96d77aa35e1d775e145ec43a4952c2d4
takasaki <takasaki@hitomedia.jp>
2017-04-03 18:14:49 +0900

    スラードショーアプリ作成（拡大画像表示は未実装）

--- SlideshowApp/ViewController.swift
+++ SlideshowApp/ViewController.swift
@@ -10,16 +10,67 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var timer: Timer!
+    var count: Int = 0
+    let imageArray: [UIImage] =
+        [UIImage(named: "one")!, UIImage(named: "two")!, UIImage(named: "three")!, UIImage(named: "four")!]
+    
+    @IBOutlet weak var slideImage: UIImageView!
+    @IBOutlet weak var startStopButton: UIButton!
+    @IBOutlet weak var prevButton: UIButton!
+    @IBOutlet weak var backButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        slideImage.image = imageArray[count]
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func prev(_ sender: Any) {
+        if (count == imageArray.count - 1) {
+            count = 0
+        } else {
+            count+=1
+        }
+        slideImage.image = imageArray[count]
+    }
 
+    @IBAction func back(_ sender: Any) {
+        if (count == 0) {
+            count = imageArray.count - 1
+        } else {
+            count-=1
+        }
+        slideImage.image = imageArray[count]
+    }
+    
+    @IBAction func start_stop(_ sender: Any) {
+        if self.startStopButton.titleLabel!.text == "再生" {
+            if self.timer == nil {
+                self.timer = Timer.scheduledTimer(timeInterval: 0.2, target: self, selector: #selector(prev),userInfo: nil, repeats: true)
+            }
+            // 進む、戻るボタンをタップ不可にする
+            self.prevButton.isEnabled = false
+            self.backButton.isEnabled = false
+            
+            // ボタン名を変更
+            self.startStopButton.setTitle("停止", for: UIControlState.normal)
+        } else {
+            if self.timer != nil {
+                self.timer.invalidate()
+                self.timer = nil
+            }
+            // 進む、戻るボタンをタップ可にする
+            self.prevButton.isEnabled = true
+            self.backButton.isEnabled = true
+            
+            // ボタン名を変更
+            self.startStopButton.setTitle("再生", for: UIControlState.normal)
+        }
+    }
 }
 
